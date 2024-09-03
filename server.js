5474669a798855676cfd4880c8da6b51b4d1d059
pohjolanetwork <arinaahh@proton.me>
2024-09-03 15:51:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 443;
 
 // Setup for serving static files
 app.use(express.static('public'));
